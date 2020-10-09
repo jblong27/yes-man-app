b102834253c0054f2fdbad766935faf091033a7e
Jordan Long <jblong27@gmail.com>
2020-10-08 20:37:15 -0500

    adding questionnaire component

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Question from './components/Question';
+
+function App() {
+  return (
+
+    <div className="App">
+      <div className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>Yes Man! Questionnaire</h2>
+      </div>
+      <Question content="" />
+    </div>
+  );
+}
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,6 @@
-import React from 'react';
-import {render, ReactDOM} from 'react-dom';
-import {IntlProvider} from 'react-intl';
-import './stylesheets/index.css';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import {AppContainer} from 'react-hot-loader';
-import {Provider} from 'react-redux';
-import {initialState} from './store/config'
+ReactDOM.render(<App />, document.getElementById("root"));
 
-const store = configureStore(initialState);
-
-const renderApp = () => (
-
-  <IntlProvider locale="en" messages={LocaleData}>
-      <Provider store={store}>
-          <AppContainer>
-              <App />
-          </AppContainer>
-      </Provider>
-  </IntlProvider>
-
-);
-
-function runOnLoaded() {
-  render(renderApp(), document.getElementById('root'));
-}
-
-const loadedStates = ['complete', 'loaded', 'interactive'];
-
-if (loadedStates.includes(document.readyState) && document.body) {
-  runOnLoaded();
-} else {
-  window.addEventListener('DOMContentLoaded', runOnLoaded, false);
-}
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
