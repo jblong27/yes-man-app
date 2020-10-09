999064ea2696c6934e5a4e4f65beb87297b1ab0f
Jordan Long <jblong27@gmail.com>
2020-10-08 19:10:06 -0500

    adding question, questioncount and answeroption components

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Question from '../components/Question';
+
+ReactDOM.render(
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Yes Man! Questionnaire</h2>
     </div>
-  );
-}
-
-export default App;
+    <Question content="" />
+  </div>
+);
---
+++ client/src/components/AnswerOption.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AnswerOption(props) {
+  return (
+    <li className="answerOption">
+      <input
+        type="radio"
+        className="radioCustomButton"
+        name="radioGroup"
+        checked={props.answerType === props.answer}
+        id={props.answerType}
+        value={props.answerType}
+        disabled={props.answer}
+        onChange={props.onAnswerSelected}
+      />
+    </li>
+  );
+}
+
+AnswerOption.PropTypes = {
+  answerType: PropTypes.string.isRequired,
+  answerContent: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+  onAnswerSelected: PropTypes.string.isRequired
+};
+
+export default AnswerOption;
