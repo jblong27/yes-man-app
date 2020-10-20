7b6dae5ab2871405669e9c024c3d054195bac431
Jordan Long <jblong27@gmail.com>
2020-10-20 18:34:23 -0500

    firebase change

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: AIzaSyBF2H5XAQdryaYQIGq2ZgzFEIbWhbWZxp8,//process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
