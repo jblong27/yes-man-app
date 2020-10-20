85b0b4862a54e2c22b459e2f0bb7eb60db2f03e6
Jared Varnum <jvarnum3@gmail.com>
2020-10-19 19:02:42 -0500

    simplifying questionnaire for mvp

--- client/src/api/questionnaireQuestions.js
+++ client/src/api/questionnaireQuestions.js
@@ -1,27 +1,27 @@
 var questionnaireQuestions = [
   {
-    question: "Would you rather stay inside or go outside today?",
+    question: "Would you rather stay inside or go outside?",
     answers: [
       {
         type: "inside",
-        content: "Inside!"
+        content: "Inside"
       },
       {
         type: "outside",
-        content: "Outside!"
+        content: "Outside"
         }
     ]
   },
   {
-    question: "Going social or flying solo?",
+    question: "Flying solo or going social?",
     answers: [
       {
         type: "social",
-        content: "Feeling social today."
+        content: "I'm feeling social!"
       },
       {
         type: "solo",
-        content: "Solo, please."
+        content: "I'm flying solo."
       }
     ]
   },
@@ -30,184 +30,202 @@ var questionnaireQuestions = [
     answers: [
       {
         type: "money",
-        content: "Sure."
+        content: "Yes"
       },
       {
         type: "noMoney",
-        content: "Money, what money?"
+        content: "No"
       }
     ]
   },
   {
-    question: "What’s most important to you, today?",
+    question: "What kind of activity do you want?",
     answers: [
       {
         type: "fitness",
-        content: "Carpe-ing the diem."
+        content: "Fitness!"
       },
       {
         type: "learning",
-        content: "Profite de la vie."
+        content: "Learning."
       },
       {
         type: "leisure",
-        content: "Parlaying the DM’s."
-      },
+        content: "Leisure..."
+      }
     ]
-  },
-  {
-    question: "Which one would you prefer?",
-    answers: [
-      {
-        type: "fitness",
-        content: "Eating something fresh and healthy."
-      },
-      {
-        type: "learning",
-        content: "Crafting a new meal from scratch."
-      },
-      {
-        type: "leisure",
-        content: "Something quick, tasty, and easy to prepare."
-      },
-    ]
-  },
-  {
-    question: "Which one would you pick?",
-    answers: [
-      {
-        type: "fitness",
-        content: "+ 10 Strength"
-      },
-      {
-        type: "learning",
-        content: "+ 10 Intellect"
-      },
-      {
-        type: "leisure",
-        content: "+ 10 Charm"
-      },
-    ]
-  },
-  {
-    question: "Your car just experienced an electrical failure, what do you do?",
-    answers: [
-      {
-        type: "fitness",
-        content: "I try to roll the vehicle further down the road to a safe location. I get out and push the car if necessary. Once I’m in a safe place, I call for assistance. "
-      },
-      {
-        type: "learning",
-        content: "I immediately pull the vehicle over and call a tow truck.  Then, while I wait for the driver I Google how to fix the issue."
-      },
-      {
-        type: "leisure",
-        content: "I immediately pull over and call a tow truck. While I wait for the driver, I finish watching a YouTube video from earlier."
-      },
-    ]
-  },
-  {
-    question: "Which most accurately depicts your ideal beach vacation?",
-    answers: [
-      {
-        type: "fitness",
-        content: "A late evening sunset, with slowmo-Baywatch-style beach jogging."
-      },
-      {
-        type: "learning",
-        content: "Umbrella over-head, my feet in the sand with a book in my hand."
-      },
-      {
-        type: "leisure",
-        content: "Sunbathing, butt in the sand, cold beer in my hand."
-      },
-    ]
-  },
-  {
-    question: "What’s most important to you?",
-    answers: [
-      {
-        type: "fitness",
-        content: "My health."
-      },
-      {
-        type: "learning",
-        content: "The community around me."
-      },
-      {
-        type: "leisure",
-        content: "My job."
-      },
-    ]
-  },
-  {
-    question: "What mirrors your mood most closely?",
-    answers: [
-      {
-        type: "fitness",
-        content: "Spontaneous."
-      },
-      {
-        type: "learning",
-        content: "Curious."
-      },
-      {
-        type: "leisure",
-        content: "Chill."
-      },
-    ]
-  },
-  {
-    question: "What is the greatest obstacle you face right now?",
-    answers: [
-      {
-        type: "fitness",
-        content: "The fear of failure."
-      },
-      {
-        type: "learning",
-        content: "The fear of the unknown."
-      },
-      {
-        type: "leisure",
-        content: "The fear of being misunderstood."
-      },
-    ]
-  },
-  {
-    question: "What would interest you most?",
-    answers: [
-      {
-        type: "fitness",
-        content: "Playing sports."
-      },
-      {
-        type: "learning",
-        content: "Solving puzzles."
-      },
-      {
-        type: "leisure",
-        content: "Taking shots."
-      },
-    ]
-  },
-  {
-    question: "Pick a number - any number!",
-    answers: [
-      {
-        type: "fitness",
-        content: "30"
-      },
-      {
-        type: "learning",
-        content: "42"
-      },
-      {
-        type: "leisure",
-        content: "420"
-      },
-    ]
-  },
-];
+  }
+]
 
 export default questionnaireQuestions;
+  
+  // //*{
+  //   question: "What’s most important to you, today?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "Carpe-ing the diem."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "Profite de la vie."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "Parlaying the DM’s."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Which one would you prefer?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "Eating something fresh and healthy."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "Crafting a new meal from scratch."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "Something quick, tasty, and easy to prepare."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Which one would you pick?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "+ 10 Strength"
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "+ 10 Intellect"
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "+ 10 Charm"
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Your car just experienced an electrical failure, what do you do?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "I try to roll the vehicle further down the road to a safe location. I get out and push the car if necessary. Once I’m in a safe place, I call for assistance. "
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "I immediately pull the vehicle over and call a tow truck.  Then, while I wait for the driver I Google how to fix the issue."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "I immediately pull over and call a tow truck. While I wait for the driver, I finish watching a YouTube video from earlier."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Which most accurately depicts your ideal beach vacation?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "A late evening sunset, with slowmo-Baywatch-style beach jogging."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "Umbrella over-head, my feet in the sand with a book in my hand."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "Sunbathing, butt in the sand, cold beer in my hand."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "What’s most important to you?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "My health."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "The community around me."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "My job."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Which one matches your mood most?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "Spontaneous."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "Curious."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "Chill."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Which fear can you relate to most?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "The fear of failure."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "The fear of the unknown."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "The fear of being misunderstood."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "What would interest you most?",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "Playing sports."
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "Solving puzzles."
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "Taking shots."
+  //     },
+  //   ]
+  // },
+  // {
+  //   question: "Pick a number - any number!",
+  //   answers: [
+  //     {
+  //       type: "fitness",
+  //       content: "30"
+  //     },
+  //     {
+  //       type: "learning",
+  //       content: "42"
+  //     },
+  //     {
+  //       type: "leisure",
+  //       content: "420"
+  //     },
+  //   ]
+  // }
