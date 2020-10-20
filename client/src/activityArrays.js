18f61b271edbacbad276977c0d4ae04b76288d8e
Jared Varnum <jvarnum3@gmail.com>
2020-10-20 00:33:39 -0500

    adding activities into javascript array

--- client/src/activityArrays.js
+++ client/src/activityArrays.js
@@ -33,8 +33,8 @@ const insideSoloMoneyFitness = [
     approved: true,
     class: "insideSoloMoneyFitness",
     date: new Date(Date.now())
-  }];
- 
+  }
+];
 const insideSoloMoneyLearning = [
     {
     author: "Jared Varnum",
@@ -72,7 +72,6 @@ const insideSoloMoneyLearning = [
     date: new Date(Date.now())
   }
 ];
-
 const insideSoloMoneyLeisure = [
     {
     author: "Jared Varnum",
@@ -110,16 +109,277 @@ const insideSoloMoneyLeisure = [
     date: new Date(Date.now())
   }
 ];
+const insideSoloNoMoneyFitness = [
+    
+  {
+    author: "Jared Varnum",
+    activity: "Stretch!",
+    approved: true,
+    class: "InsideSoloNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Make a new playlist, and dance to it!",
+    approved: true,
+    class: "InsideSoloNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Download a free workout app!",
+    approved: true,
+    class: "InsideSoloNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Underwater basket weaving! Jk, do some full body calisthenics!",
+    approved: true,
+    class: "InsideSoloNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Try to open a 16oz bottle of Dr. Pepper, but don't drink it! That's will power!",
+    approved: true,
+    class: "InsideSoloNoMoneyFitness",
+    date: new Date(Date.now())
+  }
 
-const insideSoloNoMoneyFitness = [];
-const insideSoloNoMoneyLearning = [];
-const insideSoloNoMoneyLeisure = [];
+];
+const insideSoloNoMoneyLearning = [
+    {
+    author: "Jared Varnum",
+    activity: "Make some delicious food and kick back.",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Research random facts about the United States. Trivia night, here we come!",
+    approved: true,
+    class: "insideSoloNoMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Learn about investing.",
+    approved: true,
+    class: "insideSoloNoMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go to your local library and search the non-fiction section!",
+    approved: true,
+    class: "insideSoloNoMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Research Tesla.",
+    approved: true,
+    class: "insideSoloNoMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Get on Reddit and follow a cooking subreddit!",
+    approved: true,
+    class: "insideSoloNoMoneyLearning",
+    date: new Date(Date.now())
+  }
+];
+const insideSoloNoMoneyLeisure = [
+  {
+    author: "Jared Varnum",
+    activity: "Meditate.",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Watch Netflix and actually chill...",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Write down your thoughts! Journal where you're at in life, or write a poem or song about how you feel.",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Contemplate the significance of the Number 42.",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go to the library and get a new fictional book!",
+    approved: true,
+    class: "InsideSoloNoMoneyLeisure",
+    date: new Date(Date.now())
+  }
+];
+const insideSocialMoneyFitness = [
+    {
+    author: "Jared Varnum",
+    activity: "Go iceskating with friends, or make new ones!",
+    approved: true,
+    class: "insideSocialMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go indoor rock climbing with friends!",
+    approved: true,
+    class: "insideSocialMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Sign up for some virtual dance lessons!",
+    approved: true,
+    class: "insideSocialMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Take a friend to the gym - Or make new friends there!",
+    approved: true,
+    class: "insideSocialMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go to a local YMCA, and go swimming! ",
+    approved: true,
+    class: "insideSocialMoneyFitness",
+    date: new Date(Date.now())
+  }
+];
+const insideSocialMoneyLearning = [
+    {
+    author: "Jared Varnum",
+    activity: "Take an online computer coding course!",
+    approved: true,
+    class: "insideSocialMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go on to Amazon and buy a book related to something you would like to learn more about!",
+    approved: true,
+    class: "insideSocialMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Research how to build something that would be a nice addition to your home. Step  1, buy the materials to make two of these items. Step 2, build the first one and gift to a friend. Step 3, build and keep the second one you make for your own home.",
+    approved: true,
+    class: "insideSocialMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Join a virtual toastmasters!",
+    approved: true,
+    class: "insideSocialMoneyLearning",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Join an investing channel on Discord.",
+    approved: true,
+    class: "insideSocialMoneyLearning",
+    date: new Date(Date.now())
+  }
 
-const insideSocialMoneyFitness = [];
-const insideSocialMoneyLearning = [];
-const insideSocialMoneyLeisure = [];
+];
+const insideSocialMoneyLeisure = [
+    {
+    author: "Jared Varnum",
+    activity: "Participate in a virtual escape room with friends!",
+    approved: true,
+    class: "insideSocialMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go to the local mall and buy yourself something nice!",
+    approved: true,
+    class: "insideSocialMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Go with friends and check out a local bar that you've never been to before",
+    approved: true,
+    class: "insideSocialMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Hungry? Go check out a local restaurant in town!",
+    approved: true,
+    class: "insideSocialMoneyLeisure",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Take the local transit, and do some good ole fashioned people watching!",
+    approved: true,
+    class: "insideSocialMoneyLeisure",
+    date: new Date(Date.now())
+  }
 
-const insideSocialNoMoneyFitness = [];
+];
+
+const insideSocialNoMoneyFitness = [
+    {
+    author: "Jared Varnum",
+    activity: "Play a game of twister! But hey, don't get your hopes up.",
+    approved: true,
+    class: "insideSocialNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Make love!",
+    approved: true,
+    class: "insideSocialNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "Play some indoor basketball at your local YMCA!",
+    approved: true,
+    class: "insideSocialNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "",
+    approved: true,
+    class: "insideSocialNoMoneyFitness",
+    date: new Date(Date.now())
+  },
+  {
+    author: "Jared Varnum",
+    activity: "",
+    approved: true,
+    class: "insideSocialNoMoneyFitness",
+    date: new Date(Date.now())
+  }
+];
 const insideSocialNoMoneyLearning = [];
 const insideSocialNoMoneyLeisure = [];
 
