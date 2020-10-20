4277afdac32e87075e02c3902ccbc2003add878e
Jordan Long <jblong27@gmail.com>
2020-10-20 01:02:42 -0500

    changes to routes, models and controllers

---
+++ models/userInput.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userInputSchema = new Schema({
+  author: { type: String, required: true },
+  activity: { type: String.trim(), required: true },
+  approved: { type: Boolean, default: false },
+  class: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+const userInput = mongoose.model("userinput", userInputSchema);
+
+module.exports = userInput;
